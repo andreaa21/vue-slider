a81f686abdd54e9420600119ba0ab4ac8dc2e989
andreaa21 <andrea.zollini@proton.me>
2022-11-03 15:13:59 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+const items = [
+    {
+        title: 'Argentina',
+        desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, perspiciatis',
+        link: 'argentina.jpg'
+    },
+    {
+        title: 'Chile',
+        desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        link: 'chile.jpg'
+    },
+    {
+        title: 'Colombia',
+        desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores',
+        link: 'colombia.jpg'
+    },
+    {
+        title: 'Peru',
+        desc: ' Lorem ipsum dolor sit amet consectetur adipisicing',
+        link: 'peru.jpg'
+    },
+    {
+        title: 'Sweden',
+        desc: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, perspiciatis parole in latino',
+        link: 'sweden.jpg'
+    }
+]
+
+
